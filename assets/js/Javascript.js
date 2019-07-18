baef2560c0ed13fddf930e7d48f8f0131e997d5d
Allaa <akhayat21@yahoo.com>
2019-07-18 16:59:00 -0400

    functional search and display button

--- assets/js/Javascript.js
+++ assets/js/Javascript.js
@@ -1,19 +1,17 @@
-
-
-
-  
-
 $(".imgSearchButton").click(function(){
     var query = $(".imgSearchInput").val()
 
     var settings = {
-        "url": "https://api.unsplash.com/photos/?per_page=25&client_id=2162c09ca7ae984caf1bc3f5d7e744f735982861bba651eead6dfe374d346b09&query="+query,
+        "url": "https://api.unsplash.com/search/photos/?per_page=25&page=1&client_id=2162c09ca7ae984caf1bc3f5d7e744f735982861bba651eead6dfe374d346b09&query="+query,
         "method": "GET",
       }
 
     $.ajax(settings).done(function (response) {
-        response.array.forEach(element => {
-            var imgLink = element.url.full
+        console.log(settings.url)
+        console.log(response)
+        response.results.forEach(element => {
+            var imgLink = "<img alt='"+element.alt_description+"' src='"+element.urls.thumb+"'>";
+            $(".resultComponent").append(imgLink)
         });
       });
 })
