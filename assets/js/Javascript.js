63760e6b38b8659455208d53532844472837aab2
Allaa <akhayat21@yahoo.com>
2019-07-18 17:09:02 -0400

    added enter button press functionality

--- assets/js/Javascript.js
+++ assets/js/Javascript.js
@@ -1,5 +1,5 @@
-$(".imgSearchButton").click(function(){
-    var query = $(".imgSearchInput").val()
+function runQuery(){
+  var query = $(".imgSearchInput").val()
 
     var settings = {
         "url": "https://api.unsplash.com/search/photos/?per_page=25&page=1&client_id=2162c09ca7ae984caf1bc3f5d7e744f735982861bba651eead6dfe374d346b09&query="+query,
@@ -7,11 +7,19 @@ $(".imgSearchButton").click(function(){
       }
 
     $.ajax(settings).done(function (response) {
-        console.log(settings.url)
-        console.log(response)
         response.results.forEach(element => {
             var imgLink = "<img alt='"+element.alt_description+"' src='"+element.urls.thumb+"'>";
             $(".resultComponent").append(imgLink)
         });
       });
+}
+
+$(".imgSearchButton").click(function(){
+  runQuery()
 })
+
+$(document).on('keypress',function(e) {
+  if(e.which == 13) {
+    runQuery()
+  }
+});
