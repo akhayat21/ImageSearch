b69b6927f667c615f05827dae5ead49c9d3f8bde
Allaa <akhayat21@yahoo.com>
2019-07-19 13:58:22 -0400

    added instructions

--- assets/js/Javascript.js
+++ assets/js/Javascript.js
@@ -5,7 +5,8 @@ function runQuery(){
       "url": "https://api.unsplash.com/search/photos/?per_page=25&page=1&client_id=2162c09ca7ae984caf1bc3f5d7e744f735982861bba651eead6dfe374d346b09&query="+query,
       "method": "GET",
     }
-
+    
+  $(".insText").html("");
   $(".resultComponent").html("");
 
   $.ajax(settings).done(function (response) {
